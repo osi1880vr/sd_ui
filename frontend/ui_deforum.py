9fcaa91925233d139e645d6ce3c3d1b44f835a20
Werner Oswald <osiworx@gmail.com>
2022-12-18 17:51:23 +0100

    print the magic list

--- frontend/ui_deforum.py
+++ frontend/ui_deforum.py
@@ -160,20 +160,18 @@ class Deforum_UI(QObject):
             print(args_dict)
             pairs = [[(k, v) for v in args_dict[k]] for k in args_dict]
             arg_combinations = list(itertools.product(*pairs))
-            print('arg_combinations', arg_combinations)
             for set in arg_combinations:
                 for arg in set:
                     name = arg[0]
                     value = arg[1]
                     if name == 'aesthetic_weight': gs.aesthetic_weight = float(value)
                     if name == 'T': gs.T = int(value)
-                    if name == 'selected_aesthetic_embedding': gs.selected_aesthetic_embedding = str(value)
+                    if name == 'selected_aesthetic_embedding': gs.diffusion.selected_aesthetic_embedding = str(value)
                     if name == 'slerp': gs.slerp = bool(value)
                     if name == 'aesthetic_imgs_text': gs.aesthetic_imgs_text = str(value)
                     if name == 'slerp_angle': gs.slerp_angle = float(value)
                     if name == 'aesthetic_text_negative': gs.aesthetic_text_negative = str(value)
                     if name == 'gradient_scale': gs.lr = float(value)
-
                     if name in ints_vals:
                         value = int(value)
                     if name in float_vals:
